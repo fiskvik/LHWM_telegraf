924bcacd40f014a4ad87934af71a61433eaab50e
fiskvik <1080388+fiskvik@users.noreply.github.com>
2025-01-11 21:56:30 +0100

    Fixed embarrassing errors

--- LHWM_telegraf.ps1
+++ LHWM_telegraf.ps1
@@ -26,11 +26,10 @@ catch {
 #Write-Host "Path: $scriptPath"
 . $scriptPath\UpdateVisitor.ps1
 
-
+$strings = "Name", "Index", "HardwareType", "SensorType", "Value"
 
 $monitor = [LibreHardwareMonitor.Hardware.Computer]::new()
 
-#(New-Object LibreHardwareMonitor.Hardware.Computer) | Get-Member
 
 $monitor.IsBatteryEnabled = $true
 $monitor.IsControllerEnabled = $true
@@ -45,55 +44,73 @@ $monitor.IsStorageEnabled = $true
 $monitor.Open()
 
 $monitor.Accept([UpdateVisitor]::new());
-$strings = "Name", "Index", "HardwareType", "SensorType", "Value"
-
 
 foreach ($hardware in $monitor.Hardware) {
         #write-host $hardware.GetReport();
         #write-host $hardware.Identifier
 	foreach ($sensor in $hardware.Sensors) {
                 $out = [System.Text.StringBuilder]""
-                $null = $out.AppendLine("LibreHardwareMonitor")
+                $null = $out.Append("LibreHardwareMonitor")
                 foreach ($string in $strings) {
                         if (!([string]::IsNullOrWhitespace($hardware.$string))) {
-	                        $null = $out.AppendLine((",hw_$string=$hardware.$string").Replace(" ","_"))
+			        if ($string.equals("HardwareType")) {
+	                                $null = $out.Append((","+$string+"="+$hardware.$string).Replace(" ","_"))
+	                        } else {
+			                $null = $out.Append((",Hardware"+$string+"="+$hardware.$string).Replace(" ","_"))
+	                        }
 			}
+                        #write-host "string" $string
+                        #write-host "sensor.string" $sensor.$string
                 }
                 foreach ($string in $strings) {
                         if (!([string]::IsNullOrWhitespace($sensor.$string))) {
-			        if $string.equals("Value") {
-	                                $null = $out.AppendLine((" $string=$hardware.$string").Replace(" ","_"))
+			        if ($string.equals("Value")) {
+	                                $null = $out.Append(" " + $sensor.$string)
+	                        } elseif ($string.equals("SensorType")) {
+			                $null = $out.Append((","+$string+"="+$sensor.$string).Replace(" ","_"))
 	                        } else {
-			                $null = $out.AppendLine((",$string=$hardware.$string").Replace(" ","_"))
-	                        }
+                                        $null = $out.Append((",Sensor"+$string+"="+$sensor.$string).Replace(" ","_"))
+                                }
 			}
                 }
+                write-host $out.ToString()
         }
         foreach ($subHardware in $hardware.SubHardware) {
                 #write-host $hardware.GetReport()
                 #write-host "Subhardware:" $subHardware.Name;
 	        foreach ($sensor in $subHardware.Sensors) {
                         $out = [System.Text.StringBuilder]""
-                        $null = $out.AppendLine("LibreHardwareMonitor")
+                        $null = $out.Append("LibreHardwareMonitor")
                         foreach ($string in $strings) {
                                 if (!([string]::IsNullOrWhitespace($hardware.$string))) {
-	                                $null = $out.AppendLine((",hw_$string=$hardware.$string").Replace(" ","_"))
+			                if ($string.equals("HardwareType")) {
+	                                        $null = $out.Append((","+$string+"="+$hardware.$string).Replace(" ","_"))
+	                                } else {
+			                        $null = $out.Append((",Hardware"+$string+"="+$hardware.$string).Replace(" ","_"))
+	                                }
 		        	}
                         }
                         foreach ($string in $strings) {
-                                if (!([string]::IsNullOrWhitespace($hardware.$string))) {
-	                                $null = $out.AppendLine((",subhw_$string=$hardware.$string").Replace(" ","_"))
+                                if (!([string]::IsNullOrWhitespace($subhardware.$string))) {
+			                if ($string.equals("HardwareType")) {
+	                                        $null = $out.Append((",Sub"+$string+"="+$subhardware.$string).Replace(" ","_"))
+	                                } else {
+			                        $null = $out.Append((",SubHardware"+$string+"="+$subhardware.$string).Replace(" ","_"))
+	                                }
 		        	}
                         }
                         foreach ($string in $strings) {
                                 if (!([string]::IsNullOrWhitespace($sensor.$string))) {
-	        		        if $string.equals("Value") {
-	                                        $null = $out.AppendLine((" $string=$hardware.$string").Replace(" ","_"))
+	        		        if ($string.equals("Value")) {
+	                                        $null = $out.Append(" "+$sensor.$string)
+	                                } elseif ($string.equals("SensorType")) {
+			                        $null = $out.Append((","+$string+"="+$sensor.$string).Replace(" ","_"))
 	                                } else {
-		        	                $null = $out.AppendLine((",$string=$hardware.$string").Replace(" ","_"))
+                                                $null = $out.Append((",Sensor"+$string+"="+$sensor.$string).Replace(" ","_"))
 	                                }
 	        		}
                         }
+                        write-host $out.ToString()
 	        }
 	}
 }
